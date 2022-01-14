7267bea72b576b04f375843d00cfbe4b749829ec
“theangelofdead01” <rockinthecity142@gmail.com>
2022-01-13 21:52:01 -0500

    superior-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_alioth-user \
-    aosp_alioth-userdebug \
-    aosp_alioth-eng
+    superior_alioth-user \
+    superior_alioth-userdebug \
+    superior_alioth-eng
--- superior_alioth.mk
+++ superior_alioth.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
@@ -17,18 +17,13 @@ TARGET_USES_BLUR := true
 #USE_PIXEL_CHARGING := true
 TARGET_BOOT_ANIMATION_RES := 1080
 #USE_AOSP_CLOCK := true
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := superior_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-
-Elixir_MAINTAINER := ∂α૨ҡ_ρɦσεɳเא
-IS_PHONE := true
-CUSTOM_BUILD_TYPE := OFFICIAL
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_GAPPS_ARCH := arm64
+BUILD_WITH_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
